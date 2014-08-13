bed00477695e61ce3f536e05e837b15909173051
Vegard Løkken <vegard@headspin.no>
2014-08-13 14:24:30 +0200

    Rewrite to make it handle functions as conditions
    Make it handle function conditions that can use object properties
    for conditional checking.
    Rewrite to make it use more built in jQuery functionality.
    Remove elif() because it's too complicated at this point.

--- jquery.if-else.js
+++ jquery.if-else.js
@@ -1,44 +1,31 @@
 /**
  * jQuery if-else plugin
  *
- * Creates if(), else(), elif() and fi() methods that can be used in
+ * Creates if(), else() and fi() methods that can be used in
  * a chain of methods on jQuery objects
- * 
+ *
  * @author Vegard Løkken <vegard@headspin.no>
- * @copyright 2013
- * @version 0.5.0
+ * @copyright 2014
+ * @version 1.0.0
  * @licence MIT
  */
 ;(function( $ ) {
 
-    /* Undef object that all chained jQuery methods work on
-     * if they should not be executed */
-    var $undef = $();
-
-    /* The original object is stored here so we can restore
-     * it later */
-    var orig;
-
-    $.fn["if"] = function(condition) {
-        if (this === $undef) {
-            return $();
-        } else {
-            orig = this;
-            return !!condition ? orig : $undef;
-        }
+    var getFilter = function(condition) {
+        return (typeof condition === 'function') ?
+            condition : function() { return condition; };
     };
 
-    $.fn["elif"] = function(condition) {
-        return this.else().if(condition);
+    $.fn["if"] = function(condition) {
+        return this.filter(getFilter(condition));
     };
 
     $.fn["else"] = function() {
-        return (this === $undef || this.prevObject === $undef) ?
-	    (orig || this) : $undef;
+        return this.end().not(this);
     };
 
     $.fn["fi"] = function() {
-        return orig || this;
+        return this.end();
     };
 
 })(jQuery);
